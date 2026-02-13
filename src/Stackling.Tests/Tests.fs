573029496f6276d85780ad662425fd109a030bc8
Bas Pennings <baspster@gmail.com>
2026-02-13 18:32:10 +0100

    Implemented test helpers.

--- src/Stackling.Tests/Tests.fs
+++ src/Stackling.Tests/Tests.fs
@@ -4,99 +4,88 @@ open Stackling.Runtime
 open Stackling.Interpreter
 open Xunit
 
+let expectOk
+    (x: Result<Runtime, JoyError * TraceEntry * Runtime>)
+    (test: Runtime -> unit)=
+        match x with
+        | Ok rt -> test rt
+        | Error (err, _, _) -> Assert.Fail(err.ToString())
+
+let expectError
+    (x: Result<Runtime, JoyError * TraceEntry * Runtime>)
+    (test: JoyError * TraceEntry * Runtime -> unit) =
+        match x with
+        | Ok _ -> Assert.Fail("Expected error")
+        | Error err -> test err
+
 [<Fact>]
 let ``int literals are pushed onto the stack`` () =
     let rt0 = { defaultRuntime with Queue = [Int 1] }
-    match step rt0 with
-    | Ok rt1 ->
-        let trace = rt1.Trace.Head
-        Assert.Equal(Int 1, rt1.Stack.Head)
-        Assert.Empty(rt1.Queue)
-        Assert.Equal(None, trace.Resolution)
-    | Error x ->
-        Assert.Fail(x.ToString())
+    expectOk (step rt0) (fun rt ->
+        let trace = rt.Trace.Head
+        Assert.Equal(Int 1, rt.Stack.Head)
+        Assert.Empty(rt.Queue)
+        Assert.Equal(None, trace.Resolution))
 
 [<Fact>]
 let ``dup should duplicate value on top of stack`` () =
     let r0 = { defaultRuntime with Queue = [Int 1; Symbol "dup"] }
     let r1 = r0 |> step |> Result.bind step
-    match r1 with
-    | Ok rt ->
+    expectOk r1 (fun rt ->
         match rt.Stack with
         | x :: y :: _ ->
             Assert.Equal(Int 1, x)
-            Assert.Equal(Int 1, y)        
-        | _ ->
-            Assert.Fail("Stack is not of length 2")
-    | Error (err, _, _) ->
-        Assert.Fail(err.ToString())
+            Assert.Equal(Int 1, y)
+        | _ -> Assert.Fail("Stack is not of length 2"))
       
 [<Fact>]
 let ``literal only program`` () =
     let p = [Int 1; Int 2; Int 3]
     let rt = { defaultRuntime with Queue = p }
-    match runUntilHalt rt with
-    | Ok rt ->
+    expectOk (runUntilHalt rt) (fun rt ->
         match rt.Stack with
         | x :: y :: z :: _ ->
             Assert.Equal(Int 3, x)
             Assert.Equal(Int 2, y)
             Assert.Equal(Int 1, z)
-        | _ ->
-            Assert.Fail("Stack is not of length 3")
-    | err ->
-        Assert.Fail(err.ToString())
+        | _ -> Assert.Fail("Stack is not of length 3"))
 
 [<Fact>]
 let ``builtin only program`` () =
     let p = [Int 1; Symbol "dup"; Symbol "dup"]
     let rt = { defaultRuntime with Queue = p }
-    match runUntilHalt rt with
-    | Ok rt ->
+    expectOk (runUntilHalt rt) (fun rt ->
         match rt.Stack with
         | x :: y :: z :: _ ->
             Assert.Equal(Int 1, x)
             Assert.Equal(Int 1, y)
             Assert.Equal(Int 1, z)
-        | _ ->
-            Assert.Fail("Stack is not of length 3")
-    | err ->
-        Assert.Fail(err.ToString())
+        | _ -> Assert.Fail("Stack is not of length 3"))
     
 [<Fact>]
 let ``user-defined word expansion`` () =
     let foo = Defined [Int 1; Int 2; Symbol "dup"]
     let env = defaultEnv |> Map.add "foo" foo
     let rt = { defaultRuntime with Env = env; Queue = [Symbol "foo"] }
-    match runUntilHalt rt with
-    | Ok rt ->
-        match rt.Stack with
-        | x :: y :: z :: _ ->
-            Assert.Equal(Int 2, x)
-            Assert.Equal(Int 2, y)
-            Assert.Equal(Int 1, z)
-        | _ ->
-            Assert.Fail("Stack is not of length 3")
-    | err ->
-        Assert.Fail(err.ToString())
+    expectOk (runUntilHalt rt) (fun rt ->
+         match rt.Stack with
+         | x :: y :: z :: _ ->
+             Assert.Equal(Int 2, x)
+             Assert.Equal(Int 2, y)
+             Assert.Equal(Int 1, z)
+         | _ -> Assert.Fail("Stack is not of length 3"))
     
 [<Fact>]
 let ``error propagation`` () =
     let p = [Symbol "dup"]
     let rt = { defaultRuntime with Queue = p }
-    match runUntilHalt rt with
-    | Error (err, traceEntry, _) ->
+    expectError (runUntilHalt rt) (fun (err, traceEntry, _) ->
         Assert.Equal(Symbol "dup", traceEntry.Instruction)
-        Assert.Equal(err, StackUnderflow)
-    | _ ->
-        Assert.Fail("Expected error")
+        Assert.Equal(StackUnderflow, err))
       
 [<Fact>]
 let ``instruction is consumed on error`` () =
     let p = [Symbol "dup"]
     let tr = { defaultRuntime with Queue = p }
-    match runUntilHalt tr with
-    | Error (_, _, rt) ->
-        Assert.Empty(rt.Queue)
-    | _ ->
-        Assert.Fail("Expected error")
+    expectError (runUntilHalt tr) (fun (_, _, rt) ->
+        Assert.Empty(rt.Queue))
