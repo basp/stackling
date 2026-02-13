a438c3738e757d0f264e2e436e20e4407b6556ca
Bas Pennings <baspster@gmail.com>
2026-02-13 18:00:36 +0100

    Moved over diagnostics.

---
+++ src/Stackling/Diagnostics.fs
@@ -0,0 +1,17 @@
+﻿module Stackling.Diagnostics
+
+open Stackling.Runtime
+
+type RuntimeStateView = {
+    Queue: JoyValue list
+    Stack: JoyValue list
+    TraceSummary: JoyValue list }
+
+let initRuntimeStateView (rt: Runtime) =
+    let traceSummary =
+        rt.Trace
+        |> List.map (fun te -> te.Instruction)
+        |> List.rev
+    { Queue = rt.Queue
+      Stack = rt.Stack
+      TraceSummary = traceSummary }
